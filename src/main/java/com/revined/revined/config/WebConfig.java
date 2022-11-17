532c75e548fc3c24dfba49627a577c888c3ba3bb
Thomas Morris <thomas@clearstep.health>
2022-11-17 16:49:40 -0600

    Disable cors

--- src/main/java/com/revined/revined/config/WebConfig.java
+++ src/main/java/com/revined/revined/config/WebConfig.java
@@ -32,27 +32,28 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        String[] origins = Objects.equals(environments.getVariable("ENVIRONMENT"), "local") ?
-                new String[]{"*"} :
-                new String[]{"http://localhost:8080", "https://www.rackd.io", "https://re10shon-backend.herokuapp.com/*"};
-
-        registry
-                .addMapping("/**")
-                .allowedHeaders(
-                        HttpHeaders.AUTHORIZATION,
-                        HttpHeaders.CONTENT_TYPE,
-                        HttpHeaders.ACCEPT,
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS,
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
-                        HttpHeaders.ORIGIN,
-                        "X-Requested-With",
-                        "X-XSRF-TOKEN",
-                        "User-Rackd-Cookie"
-                )
-                .allowedOrigins(origins)
-                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
-                .allowCredentials(!Objects.equals(environments.getVariable("ENVIRONMENT"), "local"));
+//        String[] origins = Objects.equals(environments.getVariable("ENVIRONMENT"), "local") ?
+//                new String[]{"*"} :
+//                new String[]{"http://localhost:8080", "https://www.rackd.io", "https://re10shon-backend.herokuapp.com/*"};
+//
+//        registry
+//                .addMapping("/**")
+//                .allowedHeaders(
+//                        HttpHeaders.AUTHORIZATION,
+//                        HttpHeaders.CONTENT_TYPE,
+//                        HttpHeaders.ACCEPT,
+//                        HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
+//                        HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS,
+//                        HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,
+//                        HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+//                        HttpHeaders.ORIGIN,
+//                        "X-Requested-With",
+//                        "X-XSRF-TOKEN",
+//                        "User-Rackd-Cookie"
+//                )
+//                .allowedOrigins(origins)
+//                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+//                .allowCredentials(!Objects.equals(environments.getVariable("ENVIRONMENT"), "local"));
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
