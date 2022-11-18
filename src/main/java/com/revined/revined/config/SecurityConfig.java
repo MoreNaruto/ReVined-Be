72ed8bd5341a1b0c0d6a9746571fd10d0015af2d
Thomas Morris <thomas@clearstep.health>
2022-11-17 19:10:54 -0600

    Turn on cors

--- src/main/java/com/revined/revined/config/SecurityConfig.java
+++ src/main/java/com/revined/revined/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
                 .requiresSecure()
                 .and()
                 .cors()
-                .disable()
+                .and()
                 .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .ignoringAntMatchers("/authenticate", "/wine/**")
