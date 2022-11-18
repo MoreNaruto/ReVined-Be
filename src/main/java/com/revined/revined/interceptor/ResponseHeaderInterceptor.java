9befbaa8b486f134f1556a6a1ec2b6c5bb49e3b2
Thomas Morris <thomas@clearstep.health>
2022-11-17 19:04:51 -0600

    Try using port number

--- src/main/java/com/revined/revined/interceptor/ResponseHeaderInterceptor.java
+++ src/main/java/com/revined/revined/interceptor/ResponseHeaderInterceptor.java
@@ -6,12 +6,14 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+
 @Component
 public class ResponseHeaderInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
         return true;
     }
 }
